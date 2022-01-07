5afe58a1143de65be88551c54845a5ef0340c8a6
ratne <ratne>
2022-01-07 15:16:45 +0100

    first release

---
+++ README.md
@@ -0,0 +1,17 @@
+## Openwa Keap Bridge
+
+A simple bridge from keap to whatsapp
+
+## Instruction
+git clone this repo
+cd path-to-folder
+npm i
+node index.js
+
+## Keap http post
+![keap http post](https://i.imgur.com/E2MsRzb.png)
+
+##ENV
+LICENSE= the license from [openwa.dev](https://gumroad.com/a/835409011)
+
+SECRET= token 
---
+++ index.js
@@ -0,0 +1,50 @@
+const express = require('express')
+const app = express()
+const port = 3000
+const bodyParser = require('body-parser');
+const cors = require('cors')
+require('dotenv').config()
+app.use(cors());
+app.use(bodyParser.json({limit: '50mb'}));
+app.use(bodyParser.urlencoded({limit: '50mb', extended: true}));
+app.get('/', (req, res) => {
+    res.send('Whatsapp Api')
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`)
+})
+
+let waClient = {}
+const wa = require('@open-wa/wa-automate');
+
+wa.create({
+    sessionId: "keap-http",
+    multiDevice: true, //required to enable multiDevice support
+    authTimeout: 60, //wait only 60 seconds to get a connection with the host account device
+    blockCrashLogs: true,
+    disableSpins: true,
+    headless: true,
+    hostNotificationLang: 'IT_IT',
+    logConsole: true,
+    popup: true,
+    qrTimeout: 0, //0 means it will wait forever for you to scan the qr code
+    licenseKey: process.env.LICENSE
+}).then(client => start(client));
+
+function start(client) {
+    waClient = client
+}
+
+app.post('/message', (req,res)=>{
+
+    if (req.body.phone && req.body.token === process.env.SECRET) {
+        const phone = req.body.phone.replaceAll('(', '').replaceAll(')', '')
+            .replaceAll(' ','').replaceAll('-','')
+            .replaceAll('+','').concat('@c.us');
+        waClient.sendText(phone, req.body.message);
+        res.send('ok').status(200);
+    }
+    else res.send('phone missing').status(404)
+
+     })
